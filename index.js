1b8660ae50aeaabbd5ae1d736c5db8269bea7af2
lsmanoel <ls.manoel@gmail.com>
2023-06-30 09:02:58 -0300

    1 file modified

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Home Page Route'));
 
-app.get('/about', (req, res) => res.send('About Page Route'));
+app.get('/string', (req, res) => res.send('Essa é uma String'));
 
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+app.get('/json', (req, res) => {
+  res.json({
+    name: 'Sensor 1',
+    model: 'HF+',
+  });
+});
 
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
+app.get('/json-array', (req, res) => {
+  res.json([
+    {
+      name: 'Sensor 1',
+      model: 'HF+',
+    },
+    {
+      name: 'Sensor 2',
+      model: 'TcAs',
+    },
+  ]);
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
+);
